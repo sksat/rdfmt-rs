18f9f37acad8838501aa8783f2c23367a6272527
sksat <sksat@sksat.net>
2022-01-02 01:51:34 +0900

    add: generate rdjsonl test

---
+++ tests/gen_json.rs
@@ -0,0 +1,30 @@
+use rdfmt::*;
+
+#[test]
+fn gen_rdjsonl() {
+    let start = Some(Position {
+        line: Some(14),
+        column: Some(15),
+    });
+    let end = Position {
+        line: Some(14),
+        column: Some(18),
+    };
+    let rdjsonl = RdJsonl {
+        code: None,
+        source: None,
+        original_output: None,
+        suggestions: None,
+        severity: Some(Severity::Error.into()),
+        message: Some("<msg>".to_string()),
+        location: Some(Location {
+            path: Some("<file path>".to_string()),
+            range: Some(Range { start, end: None }),
+        }),
+    };
+
+    assert_eq!(
+        r#"{"location":{"path":"<file path>","range":{"start":{"column":15,"line":14}}},"message":"<msg>","severity": "ERROR"}"#,
+        serde_json::to_string(&rdjsonl).unwrap(),
+    );
+}
